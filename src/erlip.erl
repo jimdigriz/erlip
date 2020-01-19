6400319763fd5c9476549f0300fdbce1d5c7e890
Alexander Clouter <alex@digriz.org.uk>
2020-01-19 12:51:24 +0000

    shorter

--- src/erlip.erl
+++ src/erlip.erl
@@ -59,10 +59,10 @@ to_ip_range(Subnet, Mask) ->
 -spec to_integer(erlip:ip()) -> non_neg_integer().
 to_integer(IP) ->
 	IPAddress = to_ip_address(IP),
-	case size(IPAddress) of
-		4 -> lists:sum([ element(4 - (X div 8), IPAddress) bsl X || X <- lists:seq(32 - 8, -1, -8) ]);
-		8 -> lists:sum([ element(8 - (X div 16), IPAddress) bsl X || X <- lists:seq(128 - 16, -1, -16) ])
-	end.
+	lists:sum(case size(IPAddress) of
+		4 -> [ element(4 - (X div 8), IPAddress) bsl X || X <- lists:seq(32 - 8, -1, -8) ];
+		8 -> [ element(8 - (X div 16), IPAddress) bsl X || X <- lists:seq(128 - 16, -1, -16) ]
+	end).
 
 -spec version(erlip:ip()) -> 4 | 6.
 version(IP) ->
